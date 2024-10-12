31332db0b368d5bec129a03501399e44fd111b72
fl <pipecarnal123@gmail.com>
2024-10-12 05:27:44 +0000

    fixed missing .env file when pulling

--- app_deploy/deploy.sh
+++ app_deploy/deploy.sh
@@ -7,24 +7,23 @@ set -a  # Automatically export all variables
 . ../.env
 set +a  # Stop exporting
 
-# Set up variables from the .env file
-REPO_URL="${REPO_URL}"
-REPO_NAME=$(basename -s .git "$REPO_URL")  # Extract repo name from URL
-APP_BASE_DIR="$(dirname "$0")"
-APP_DIR="$APP_BASE_DIR/$REPO_NAME"  # Target directory for the repo
-DOCKER_COMPOSE_PATH="$APP_DIR/docker-compose.yml"
-
 # Ensure that REPO_URL is set
 if [ -z "$REPO_URL" ]; then
     echo "REPO_URL is not set in the environment. Exiting."
     exit 1
 fi
 
+# Set up variables from the .env file
+REPO_NAME=$(basename -s .git "$REPO_URL")  # Extract repo name from URL
+APP_BASE_DIR="$(dirname "$(realpath "$0")")"
+APP_DIR="$APP_BASE_DIR/$REPO_NAME"  # Target directory for the repo
+DOCKER_COMPOSE_PATH="$APP_DIR/docker-compose.yml"
+
 # Look for any existing repositories in app_deploy
 for DIR in "$APP_BASE_DIR"/*; do
     if [ -d "$DIR/.git" ]; then
         EXISTING_REPO_URL=$(git -C "$DIR" config --get remote.origin.url)
-        
+
         if [ "$EXISTING_REPO_URL" != "$REPO_URL" ]; then
             echo "$EXISTING_REPO_URL different from $REPO_URL"
             # Delete the directory if the URLs do not match
@@ -33,30 +32,33 @@ for DIR in "$APP_BASE_DIR"/*; do
         else
             # Repo matches; we should just pull the latest changes
             echo "Repository matches at $DIR. Pulling latest changes..."
-            cd "$DIR"
+            cd "$DIR" || exit
             git fetch origin main
             git reset --hard origin/main
             if [ $? -ne 0 ]; then
                 echo "Git pull failed. Exiting."
                 exit 1
             fi
-            # Set APP_DIR to the matched repo directory
-            APP_DIR="$DIR"
         fi
     fi
 done
 
+
+cd "$APP_BASE_DIR"
+
 # Clone the repository if it doesn't exist
 if [ ! -d "$APP_DIR" ]; then
     echo "Cloning repository from $REPO_URL into $APP_DIR..."
-    cd "$APP_BASE_DIR"
-    git clone "$REPO_URL" "$APP_DIR"
+    cd "$APP_BASE_DIR" || exit
+    git clone "$REPO_URL" "$REPO_NAME"  # Directly use REPO_NAME to avoid nested directories
     git config --global --add safe.directory "$APP_DIR"
     if [ $? -ne 0 ]; then
         echo "Failed to clone repository. Exiting."
         exit 1
     fi
 fi
+
+cp "$APP_BASE_DIR/../.env" "$APP_BASE_DIR/.env"
 
 # Stop and remove existing Docker containers
 echo "Stopping and removing existing Docker Compose containers..."
--- setup_nginx_ssl.sh
+++ setup_nginx_ssl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Ensure the script is run as root
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
